aeffc7267c6b765b918b8dbc3a08c4a2829f21a8
Severin Buhler <sevi_buehler@hotmail.com>
2025-03-03 10:18:11 +0100

    single pkarr client

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,14 +1,13 @@
-use std::{sync::{atomic::{AtomicBool, Ordering}, Arc}, time::{Duration, Instant}};
-
 use mainline::Dht;
 use pkarr::{Client, PublicKey};
+use tokio::time::Instant;
 
 use crate::published_key::PublishedKey;
 
 
 
 /// Queries the public key and returns how many nodes responded with the packet.
-pub async fn count_dht_nodes_storing_packet(pubkey: &PublicKey, client: &Dht) -> u8 {
+pub async fn count_dht_nodes_storing_packet(pubkey: &PublicKey, client: Dht) -> u8 {
     let c = client.clone();
     let p = pubkey.clone();
     let handle = tokio::task::spawn_blocking(move || {
@@ -27,9 +26,9 @@ pub async fn count_dht_nodes_storing_packet(pubkey: &PublicKey, client: &Dht) ->
 
 
 // Publishes x number of packets. Checks if they are actually available
-pub async fn publish_records(num_records: usize, thread_id: usize) -> Vec<PublishedKey> {
-    let client = Client::builder().no_relays().cache_size(0).build().unwrap();
-    let dht = mainline::Dht::client().unwrap();
+pub async fn publish_records(num_records: usize, thread_id: usize, client: Client) -> Vec<PublishedKey> {
+    let dht = client.dht().unwrap();
+    dht.bootstrapped();
     let mut records = vec![];
 
     for i in 0..num_records {
@@ -41,13 +40,10 @@ pub async fn publish_records(num_records: usize, thread_id: usize) -> Vec<Publis
             continue;
         }
         let publish_time = instant.elapsed().as_millis();
-        let found_count = count_dht_nodes_storing_packet(&key.public_key(), &dht).await;
+        let found_count = count_dht_nodes_storing_packet(&key.public_key(), dht.clone()).await;
         tracing::info!("- t{thread_id:<2} {i:>3}/{num_records} Published {} on {found_count:<2} nodes within {publish_time}ms", key.public_key());
         records.push(key);
 
-        // if ctrlc_pressed.load(Ordering::Relaxed) {
-        //     break
-        // }
     }
     records
 }
